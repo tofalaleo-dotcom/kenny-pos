df411b54337bb5b61192ea0a7baf628cfa2496a4
tofalaleo-dotcom <tofalaleo@gmail.com>
2026-07-03 05:25:42 +0700

    Update service worker cache version and logic

--- kenny-pos-upload-20260625-231155/public/sw.js
+++ kenny-pos-upload-20260625-231155/public/sw.js
@@ -1,7 +1,10 @@
-const CACHE = 'lao-pos-v2'
+const CACHE = 'lao-pos-v3'
 const ASSETS = ['/', '/index.html', '/manifest.webmanifest']
 
-self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS))))
+self.addEventListener('install', (event) => {
+  self.skipWaiting()
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)))
+})
 self.addEventListener('activate', (event) => event.waitUntil(
   caches.keys()
     .then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))
@@ -9,9 +12,9 @@ self.addEventListener('activate', (event) => event.waitUntil(
 ))
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return
-  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+  event.respondWith(fetch(event.request).then((response) => {
     const copy = response.clone()
     caches.open(CACHE).then((cache) => cache.put(event.request, copy))
     return response
-  }).catch(() => caches.match('/'))))
+  }).catch(() => caches.match(event.request).then((cached) => cached || caches.match('/'))))
 })
